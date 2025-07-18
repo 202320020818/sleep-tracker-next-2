dedf316bfa780c885d4a96edeb1923bf6c3c7361
Eshan Harshana <138109759+202320020818@users.noreply.github.com>
2025-07-18 23:56:36 +0530

    Update HomePage component to enhance user experience with personalized greeting and activity overview

--- app/page.tsx
+++ app/page.tsx
@@ -11,8 +11,49 @@ export default async function HomePage() {
     return <Guest />;
   }
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold">Welcome back, {user.firstName}!</h1>
-    </div>
+    <main className="bg-gray-100 text-gray-800 front-sans min-h-screen">
+      <div className="max-w-7xl mx-auto px-4 sm:px-8 py-16 grid grid-cols-1 md:grid-cols-2 gap-8">
+        {/* left Column */}
+        <div className="space-y-6">
+          <div className="bg-white p-6 rounded-lg shadow-md flex flex-col sm:flex-row item center sm:items-start gap-6">
+            {/* Use Image */}
+            <img
+              src={user.imageUrl}
+              alt={`${user.firstName}&#39;s profile`}
+              className="w-24 h-24 rounded-full border border-gray-300 shadow-md"
+            />
+
+            {/* User details */}
+            <div className="flex-1">
+              <h2 className="text-2xl md:text-3xl font-bold text-purple-600 mb-2">
+                Welcom Back, {user.firstName}
+              </h2>
+              <p className="text-gray-600 mb-4">
+                Here&#39;s a quick overview of your recent sleep activity.on top
+                of your data insights and manage your tasks efficiently!
+              </p>
+
+              <div className="space-y-2">
+                <p className="text-gray-600">
+                  <span className="font-semibold text-gray-800">Joined :</span>{' '}
+                  {new Date(user.createdAt).toLocaleDateString()}
+                </p>
+
+                <p className='text-gray-600'>
+                  <span className='font-semibold text-gray-800'>
+                    Last Active:
+                  </span>{' '}
+                  {user.lastActiveAt
+                    ? new Date(user.lastActiveAt).toLocaleString()
+                    : 'N/A'}
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* Placeholder for AddSleepRedord */}
+          
+        </div>
+      </div>
+    </main>
   );
 }
